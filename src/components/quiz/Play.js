f0fab6efcb2c8e749b6937b339312810367969cf
Marcel0o <okechukwu.nwosu21@gmail.com>
2020-07-13 10:59:42 +0100

    added sound

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -3,7 +3,11 @@ import { Helmet } from 'react-helmet';
 import M from 'materialize-css';
 import QuestionNumber from "./QuestionNumber"
 import questions from '../../questions.json';
-import isEmpty from '../../utils/is-empty'
+import isEmpty from '../../utils/is-empty';
+
+import correctNotification from '../../assets/audio/error3.mp3';
+import wrongNotification from '../../assets/audio/error2.mp3';
+import buttonSound from '../../assets/audio/click_2.mp3';
 
 class Play extends Component {
     constructor(props) {
@@ -23,13 +27,16 @@ class Play extends Component {
             hints: 5,
             fiftyFifty: 2,
             usedFiftyFifty: false,
+            previousRandNumber: [],
             time: {}
         };
+        this.interval = null
     }
 
     componentDidMount () {
         const { questions, currentQuestion, nextQuestion, previousQuestion} = this.state;
         this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion);
+        this.startTimer();
     }
 
     displayQuestions = (questions = this.state.questions, currentQuestion, nextQuestion, previousQuestion) => {
@@ -44,13 +51,17 @@ class Play extends Component {
                 currentQuestion,
                 nextQuestion,
                 previousQuestion,
-                answer
+                numberofQuestions: questions.length,
+                answer,
+                previousRandNumber: []
+            }, () => {
+                this.showOptions();
             });
         }
     };
 
     playButtonSound = () => {
-        document.getElementById('button-sound');
+        document.getElementById('button-sound').play();
     };
 
     handleButtonClick = (e) => {
@@ -74,18 +85,25 @@ class Play extends Component {
 
     handleOptionClick = (e) => {
        if (e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
-           this.correctAnswer();
+            setTimeout(() => {
+                document.getElementById('correct-sound').play();
+            }, 250);
+            
+            this.correctAnswer();
        } else {
-           this.wrongAnswer();
+           setTimeout(() => {
+            document.getElementById('wrong-sound').play();
+           }, 250);
+            this.wrongAnswer();
        }
     };
     
     handleQuitButtonClick = () => {
         this.playButtonSound();
         window.confirm('Are you sure you want to quit?')
-        // if (window.confirm('Are you sure you want to quit?')) {
-
-        // }
+         if (window.confirm('Are you sure you want to quit?')) {
+            this.props.history.push('/');
+        }
     };
 
     handleNextButtonClick = () => {
@@ -137,32 +155,160 @@ class Play extends Component {
         this.setState(prevState => ({
             wrongAnswers: prevState.wrongAnswers + 1,
             currentQuestionIndex: prevState.currentQuestionIndex + 1,
-            numberofAnsweredQuestions: prevState.numberofAnsweredQuestions + 1
+            numberofAnsweredQuestions: prevState.numberofAnsweredQuestions
         }));
     }
 
+    showOptions = () => {
+        const options = Array.from(document.querySelectorAll('.option'));
+
+        options.forEach(option => {
+            option.style.visibility = 'visible';
+        });
+
+        this.setState({
+            usedFiftyFifty: false
+        });
+    }
+
+    handleHints = () => {
+         if (this.state.hints > 0) {
+            const options = Array.from(document.querySelectorAll('.option'));
+            let indexOfAnswer;
+
+            options.forEach((option, index) => {
+            if (option.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
+                    indexOfAnswer = index;
+                }
+            });
+
+        while (true) {
+            const randomNumber = Math.round(Math.random() * 3);
+            if (randomNumber !== indexOfAnswer && !this.state.previousRandNumber.includes(randomNumber)) {
+                options.forEach((option, index) => {
+                    if (index === randomNumber) {
+                        option.style.visibility = 'hidden';
+                        this.setState((prevState) => ({
+                            hints: prevState.hints - 1,
+                            previousRandNumber: prevState.previousRandNumber.concat(randomNumber)
+                        }));
+                    }
+                });
+                break;
+            }
+
+        if (this.state.previousRandNumber.length >= 3) break;
+        }
+    }
+    }
+
+    handleFiftyFifty = () => {
+        if ( this.state.fiftyFifty > 0 && this.state.usedFiftyFifty === false) {
+            const options = document.querySelectorAll('.option');
+            const randomNumbers = [];
+            let indexOfAnswer;
+
+            options.forEach((option, index) => {
+                if (option.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
+                    indexOfAnswer = index;
+                }
+            });
+
+            let count = 0;
+            do {
+                const randomNumber = Math.round(Math.random() * 3);
+                if (randomNumber !== indexOfAnswer) {
+                    if (randomNumbers.length < 2 && !randomNumbers.includes(randomNumber) && !randomNumbers.includes(indexOfAnswer)) {
+                        randomNumbers.push(randomNumber);
+                        count ++;
+                    } else {
+                        while (true) {
+                            const newRandomNumber = Math.round(Math.random() * 3);
+                            if (!randomNumbers.includes(newRandomNumber) && !randomNumbers.includes(indexOfAnswer)) {
+                                randomNumbers.push(newRandomNumber);
+                                count ++;
+                                break;
+                            }
+                        }
+                    }
+                }
+        } while (count < 2);
+            options.forEach((option, index) => {
+                if (randomNumbers.includes(index)) {
+                    option.style.visibility = 'hidden';
+                }
+            });
+            this.setState( prevState => ({
+                fiftyFifty: prevState.fiftyFifty - 1,
+                usedFiftyFifty: true
+            }));
+        }
+    }
+
+    startTimer = () => {
+        const countDownTime = Date.now() + 180000;
+        this.interval = setInterval(() => {
+            const now = new Date();
+            const distance = countDownTime - now;
+
+            const minuites = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 % 60)) / 1000);
+
+            if (distance < 0) {
+                clearInterval(this.interval);
+                this.setState({
+                    time: {
+                        minuites: 0,
+                        seconds: 0
+                    }
+                }, () => {
+                    alert('Your Time is up!!');
+                    this.props.history.push('/');
+                });
+            } else {
+                this.setState({
+                    time: {
+                        minuites,
+                        seconds
+                    }
+                });
+            }
+        }, 1000);
+    }
+
+    progbar = () => {
+        
+    }
+        
+
     render() {
-        const { currentQuestion } = this.state;
+        const { currentQuestion, currentQuestionIndex, numberofQuestions, hints , fiftyFifty, time} = this.state;
         return (
         <Fragment>
             <Helmet><title>Quiz Page</title></Helmet>
+
             <Fragment>
-                
+                <audio id="correct-sound" src={correctNotification}></audio>
+                <audio id="wrong-sound" src={wrongNotification}></audio>
+                <audio id="button-sound" src={buttonSound}></audio>
             </Fragment>
             <div className="questions">
                 <div className="lifeline-container">
                     <p>
-                        <span className="lifeline">3</span><span className="mdi mdi-set-center mdi-24px lifeline-icon"></span>
+                        <span className="lifeline">{fiftyFifty}</span>
+                        <span onClick={this.handleFiftyFifty} className="mdi mdi-set-center mdi-24px lifeline-icon"></span>
                     </p>
-                    <p>
-                        <span className="lifeline">5</span><span className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span>
+                    <p>                        
+                        <span onClick={this.handleHints} className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span>
+                        <span className="lifeline">{hints}</span>
                     </p>
                 </div>
                 <div>
                     <p>
-                        <span className="left" style={{ float: 'left'}}> 1 of 5</span>
-                        <span className="right">2:00<span className="mdi mdi-clock-outline mdi-24px"></span></span>
-                    </p>
+                        <span className="left" style={{ float: 'left'}}>{currentQuestionIndex + 1} of { numberofQuestions}</span>
+                        <span className="right">{time.minuites}:{time.seconds}<span className="mdi mdi-clock-outline mdi-24px"></span></span>
+                    </p><br/>
+                    <p className="prog">{(currentQuestionIndex + 1) / numberofQuestions * 100}  </p>
                 </div>
 
                 <div>
